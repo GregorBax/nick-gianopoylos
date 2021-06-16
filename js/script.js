1fba3d84d453d726869bbbc8c3b8fade91efb932
GREGORIS BACHTSEBANOS <46354311+GregorBax@users.noreply.github.com>
2021-06-16 16:55:57 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+
+	$('#slides').superslides({
+		animation: 'fade',
+		play: 5000,
+		pagination: false
+	});
+
+	var typed = new Typed(".typed", {
+		strings: ["Computer Engineer.", "Data Scientist."],
+		typeSpeed: 90,
+		loop: false,
+		startDelay: 1000,
+		showCursor: false
+	});
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
